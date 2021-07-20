c0d8102a8ce6fcfb863e9fc47d6d6da540631630
susan.novak@yahoo.com <susan.novak@yahoo.com>
2021-07-20 15:46:24 -0500

    Added code to array.

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,16 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            
+            //1. Create and initialize an array with the following values in a single line 1, 1, 2, 3, 5, 8.
+
+            int[] integerArray = {1, 1, 2, 3, 5, 8};
+
+            //2. Loop through the array and print out each value.
+
+
+
+            //3. Modify the loop to only print the odd numbers from the array.
         }
     }
 }
