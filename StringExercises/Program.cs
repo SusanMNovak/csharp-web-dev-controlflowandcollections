14c9a143a8cbc7efc24159f6d7540c75e1acc20b
susan.novak@yahoo.com <susan.novak@yahoo.com>
2021-07-20 18:56:05 -0500

    Added string code.

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,7 +6,27 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            /*1. For this exercise, create a string for the value:
+
+                I would not, could not, in a box. I would not, could not with a fox.
+                I will not eat them in a house. I will not eat them with a mouse.*/
+
+            string str "I would not, could not, in a box. I would not, could not with a fox.
+                I will not eat them in a house. I will not eat them with a mouse.";
+
+            //2. Use the Split method to divide the string at each space and store the individual words in an array.
+
+            string[] words =str.Split(" ");
+
+            //3. Print the array of words to verify that your code works. The syntax is:
+
+            Console.WriteLine(string.Join("/", words));
+
+            //4. Repeat steps 2 and 3, but change the delimiter to split the string into separate sentences.
+
+            string[]sentences = str.Split(".");
+            Console.WriteLine(string.Join("/", sentences));
+
         }
     }
 }
