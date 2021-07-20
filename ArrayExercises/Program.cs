323e804c487fda6b4a58e5e53a4c9b1887897d21
susan.novak@yahoo.com <susan.novak@yahoo.com>
2021-07-20 16:33:03 -0500

    Addded all 3 steps to array.

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -12,10 +12,22 @@ namespace ArrayExercises
             int[] integerArray = {1, 1, 2, 3, 5, 8};
 
             //2. Loop through the array and print out each value.
+            foreach (int integer in integerArray)
 
+            {
+                Console.WriteLine(integer);
+            }
 
+           //3. Modify the loop to only print the odd numbers from the array.
 
-            //3. Modify the loop to only print the odd numbers from the array.
+            for (int i = 0; i < integerArray.Length; i++)
+            {
+                if (integerArray[i] % 2 != 0)
+
+                {
+                    Console.WriteLine(integerArray[i]);
+                }
+            }
         }
     }
 }
