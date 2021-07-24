646e488a2a15b553ff25d0b3a6765a35a1752413
susan.novak@yahoo.com <susan.novak@yahoo.com>
2021-07-23 20:25:29 -0500

    Coded Dictionart Exercise.

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryExercises
 {
@@ -7,6 +8,38 @@ namespace DictionaryExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Dictionaries!");
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            string newStudent;
+
+            //Input student names and IDs
+            Console.WriteLine("Enter dtudents' names, and ID numbers or Enter to finish:");
+            do
+            {
+                Console.WriteLine("Student Name: ");
+                newStudent = Console.ReadLine();
+
+                if (newStudent != "")
+                {
+                    Console.WriteLine("ID number: ");
+                    int newID = int.Parse(Console.ReadLine());
+                    students.Add(newID, newStudent);
+
+                }
+
+            }
+
+            //Print class roster
+            while (newStudent != "");
+
+            Console.WriteLine("\nClass roster:");
+
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                Console.WriteLine(student.Value + "'s ID: " + student.Key);
+            }
+
+            Console.WriteLine("Number of students in roster: " + students.Count);
+
         }
     }
 }
